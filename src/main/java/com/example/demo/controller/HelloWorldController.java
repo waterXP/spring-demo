b04abc822b84d24c57e5b7b34792d0959b58d4cb
陈文杰 <wutianba@sina.com>
2019-05-06 16:12:04 +0800

    first

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by chenwenjie on 2019/5/5.
+ *
  */
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index () {
-        return "Hello world";
+        return "Hello world!";
     }
 }
